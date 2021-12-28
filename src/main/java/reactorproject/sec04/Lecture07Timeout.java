b22741b38719e9ad8702757e63a11d018e08d510
bthank <bthankachan@yahoo.com>
2021-12-27 20:10:50 -0500

    Reactor operator timeout

---
+++ src/main/java/reactorproject/sec04/Lecture07Timeout.java
@@ -0,0 +1,32 @@
+package reactorproject.sec04;
+
+import java.time.Duration;
+
+import reactor.core.publisher.Flux;
+import reactorproject.courseutil.Util;
+
+public class Lecture07Timeout {
+
+	
+	public static void main(String[] args) {
+		
+		getOrderNumbers()
+			.timeout(Duration.ofSeconds(2), fallback())
+			.subscribe(Util.subscriber());
+		
+		Util.sleepSeconds(60);
+		
+		
+	}
+	
+	private static Flux<Integer> getOrderNumbers() {
+		return Flux.range(1, 10)
+				.delayElements(Duration.ofSeconds(1));
+				
+	}
+	
+	private static Flux<Integer> fallback() {
+		return Flux.range(100, 10)
+				.delayElements(Duration.ofMillis(200));
+	}
+}
