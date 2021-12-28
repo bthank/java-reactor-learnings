216d714722e878ec12b7c09a3eb55a6b5ba6a2a4
bthank <bthankachan@yahoo.com>
2021-12-27 20:03:52 -0500

    Reactor error handling

---
+++ src/main/java/reactorproject/sec04/LectureOnError.java
@@ -0,0 +1,28 @@
+package reactorproject.sec04;
+
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+import reactorproject.courseutil.Util;
+
+public class LectureOnError {
+
+	public static void main(String[] args) {
+		
+		
+		Flux.range(1, 10)
+			.log()
+			.map(i -> 10 / (5 - i))
+			// .onErrorReturn(-1)
+			//.onErrorResume(e -> fallback())
+			.onErrorContinue((err, obj) -> {
+				
+			})
+			.subscribe(Util.subscriber());
+	}
+	
+	private static Mono<Integer> fallback() {
+		return Mono.fromSupplier(() -> Util.faker().random().nextInt(100, 200));
+	}
+	
+	
+}
